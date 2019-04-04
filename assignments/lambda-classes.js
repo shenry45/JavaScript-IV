248fe9110c8af44eb5102bd23fe93e91b93df9db
shenry45 <shawn45henry@gmail.com>
2019-04-03 21:20:39 -0700

    finished class testing and correct bugs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,7 +1,7 @@
 // CODE here for your Lambda Classes
 class Person {
 
-  constructor() {
+  constructor(name, age, location, gender) {
     this.name = name;
     this.age = age;
     this.location = location;
@@ -15,41 +15,41 @@ class Person {
 
 // Instructors object ---------------------------------------
 
-class Instructors extends Person {
+class Instructor extends Person {
 
-  constructor(name, age, location, gender, favLanguage, catchPhrase) {
-    super(name, age, location, gender);
+  constructor(attr) {
+    super(attr.name, attr.age, attr.location, attr.gender);
     
-    this.specialty = specialty;
-    this.favLanguage = favLanguage;
-    this.catchPhrase = catchPhrase;
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
   }
 
   demo(subject) {
     console.log(`Today we are learning about ${subject}`);
   }
-  grade(studObj, studStr) {
+  grade(student, subject) {
     console.log(`${student.name} receives a perfect score on ${subject}`);
   }
 }
 
 // Studets Object -------------------------------------------
 
-class Students extends Person {
+class Student extends Person {
 
-  constructor(name, age, location, gender) {
-    super(name, age, location, gender);
+  constructor(attr) {
+    super(attr.name, attr.age, attr.location, attr.gender);
 
-    this.previousBackground = prevBg;
-    this.className = className;
-    this.favSubjects = favSubjects;
+    this.previousBackground = attr.previosBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
     
   }
 
   listsSubjects() {
-    favSubjects.forEach(el => console.log(el));
+    this.favSubjects.forEach(el => console.log(el));
   }
-  PRAssignment(subj) {
+  PRAssignment(subject) {
     console.log(`${this.name} has submitted a PR for ${subject}`);
   }
   sprintChallenge(subj) {
@@ -59,17 +59,99 @@ class Students extends Person {
 
 // PM Object ---------------------------------------
 
-class ProjectManagers extends Instructors {
+class ProjectManager extends Instructor {
 
-  constructor(name, age, location, gender, favLanguage, catchPhrase) {
+  constructor(name, age, location, gender, favLanguage, catchPhrase, gradClassName, favInstructor) {
     super(name, age, location, gender, favLanguage, catchPhrase);
     this.gradClassName = gradClassName;
     this.favInstructor = favInstructor;
   }
-  standUp(slackChan, logs){
+  standUp(slackChan){
     console.log(`${this.name} announces to ${slackChan}, @channel standy times!`);
   }
   debugsCode(studentObj, subj) {
-    console.log(`${this.name} debugs ${studentObj}'s code on ${subj}`);
+    console.log(`${this.name} debugs ${studentObj.name}'s code on ${subj}`);
   }
 }
+
+/* ------------------------------------------------
+  OBJECT INSTANCE CREATION
+------------------------------------------------- */
+
+const bill = new Instructor({
+  name: 'Bill Harper',
+  age: 44,
+  location: 'Cabin in the Woods',
+  gender: 'M',
+  specialty: 'Making boss food',
+  favLanguage: 'French',
+  catchPhrase: 'Wabbaluba Dub Dub!'
+});
+const connor = new Instructor({
+  name: 'Connor Jones',
+  age: 30,
+  location: 'Miami, FL',
+  gender: 'M',
+  specialty: 'Python',
+  favLanguage: 'Ruby',
+  catchPhrase: 'That\'s all folks'
+});
+const michelle = new Instructor({
+  name: 'Michelle York',
+  age: 49,
+  location: 'New Orleans, LA',
+  gender: 'F',
+  specialty: 'Making boss food',
+  favLanguage: 'French',
+  catchPhrase: 'Wabbaluba Dub Dub!'
+});
+
+// bill.demo('History');
+// console.log(michelle.catchPhrase);
+
+const shawn = new Student({
+  name: 'Shawn Henry',
+  age: 22,
+  location: 'Middle of the Desert',
+  gender: 'M',
+  previousBackground: 'Wordpress Websites',
+  className: 'Full Stack Web Development',
+  favSubjects: [ 'CSS', 'HTML', 'JavaScript' ]
+});
+const adrian = new Student({
+  name: 'Adrian Nairda',
+  age: 35,
+  location: 'San Jose, CA',
+  gender: 'M',
+  previousBackground: 'Waiter',
+  className: 'Full Stack Web Development',
+  favSubjects: [ 'PHP', 'HTML', 'Markup' ]
+});
+
+// shawn.listsSubjects();
+// adrian.PRAssignment('Trigonometry');
+// connor.grade(shawn, shawn.className)
+
+const grangerD = new ProjectManager({
+  name: 'Granger Downtown',
+  age: 54,
+  location: 'Marlboro, SC',
+  gender: 'M',
+  favLanguage: 'SQL',
+  catchPhrase: 'Back in my day...',
+  gradClassName: 'WEBPT5',
+  favInstructor: 'Josh Knell'
+});
+const louiseV = new ProjectManager({
+  name: 'Louise Vernello',
+  age: 40,
+  location: 'Brownington, IL',
+  gender: 'F',
+  favLanguage: 'React',
+  catchPhrase: 'Go Lambda',
+  gradClassName: 'WEBPT5',
+  favInstructor: 'Josh Knell'
+});
+
+// grangerD.debugsCode(shawn, 'Judicial Powers');
+// louiseV.standUp('webpt5_sprint03');
